3705bb7720bcf3e5ad6bffd178491d48828c10af
Karel Resl <karelresl916@gmail.com>
2025-07-18 06:39:35 +0200

    Update pong.asm

--- pong.asm
+++ pong.asm
@@ -1,29 +1,29 @@
-; pong.asm - 64-bitová verze hry Pong s raylib
-; Kompilace: nasm -f elf64 pong.asm -o pong.o && gcc pong.o -o pong -lraylib -lm -no-pie
+; pong.asm - 64-bit Pong game with raylib
+; Compile with: nasm -f elf64 pong.asm -o pong.o && gcc pong.o -o pong -lraylib -lm -no-pie
 
 section .data
-    ; Definice kláves z raylib
+    ; Raylib key codes
     KEY_W      equ 87
     KEY_S      equ 83
     KEY_UP     equ 265
     KEY_DOWN   equ 264
     
-    ; Raylib funkce
+    ; Raylib functions
     extern InitWindow, CloseWindow, WindowShouldClose
     extern BeginDrawing, EndDrawing, ClearBackground
     extern DrawRectangle, DrawText, DrawFPS
     extern SetTargetFPS, GetScreenWidth, GetScreenHeight
     extern IsKeyDown
     
-    ; Barvy
+    ; Colors
     BLACK      equ 0xFF000000
     WHITE      equ 0xFFFFFFFF
     
-    ; Textové řetězce
-    title db "Pong v x86-64 Assembly + raylib", 0
+    ; Strings
+    title db "Pong in x86-64 Assembly with raylib", 0
     score_format db "%d", 0
     
-    ; Herní proměnné
+    ; Game variables
     screen_width dd 800
     screen_height dd 450
     paddle_width dd 20
@@ -33,47 +33,44 @@ section .data
     player1_x dd 20
     player1_y dd 175
     player1_score dd 0
+    player1_text db "0", 0
     
     player2_x dd 760
     player2_y dd 175
     player2_score dd 0
+    player2_text db "0", 0
     
     ball_x dd 400
     ball_y dd 225
     ball_speed_x dd 4
     ball_speed_y dd 4
 
-section .bss
-    score_text resb 16
-
 section .text
     global main
-    
-    ; Pro printf
     extern printf, sprintf
 
 main:
     push rbp
     mov rbp, rsp
     
-    ; Inicializace okna (InitWindow(width, height, title))
+    ; Initialize window
     mov edi, [screen_width]
     mov esi, [screen_height]
     mov rdx, title
     call InitWindow
     
-    ; Nastavení FPS (SetTargetFPS(60))
+    ; Set FPS
     mov edi, 60
     call SetTargetFPS
 
 .game_loop:
-    ; Kontrola zavření okna
+    ; Check if window should close
     call WindowShouldClose
     test eax, eax
     jnz .close_window
     
-    ; --- Vstup ---
-    ; Hráč 1 (W/S)
+    ; --- Player input ---
+    ; Player 1 (W/S keys)
     mov edi, KEY_W
     call IsKeyDown
     test eax, eax
@@ -87,7 +84,7 @@ main:
     jz .check_up
     add dword [player1_y], 5
     
-    ; Hráč 2 (šipky nahoru/dolů)
+    ; Player 2 (Up/Down arrows)
 .check_up:
     mov edi, KEY_UP
     call IsKeyDown
@@ -102,7 +99,7 @@ main:
     jz .move_ball
     add dword [player2_y], 5
     
-    ; --- Pohyb míčku ---
+    ; --- Ball movement ---
 .move_ball:
     mov eax, [ball_speed_x]
     add [ball_x], eax
@@ -110,15 +107,15 @@ main:
     mov eax, [ball_speed_y]
     add [ball_y], eax
     
-    ; --- Kolize s hranami ---
-    ; Horní stěna
+    ; --- Wall collisions ---
+    ; Top wall
     mov eax, [ball_y]
     cmp eax, 0
     jg .check_bottom
     neg dword [ball_speed_y]
     
 .check_bottom:
-    ; Dolní stěna
+    ; Bottom wall
     mov eax, [ball_y]
     mov ebx, [screen_height]
     sub ebx, [ball_size]
@@ -126,9 +123,9 @@ main:
     jl .check_paddle1
     neg dword [ball_speed_y]
     
-    ; --- Kolize s pádly ---
+    ; --- Paddle collisions ---
 .check_paddle1:
-    ; Levé pádlo
+    ; Left paddle
     mov eax, [ball_x]
     mov ebx, [player1_x]
     add ebx, [paddle_width]
@@ -147,7 +144,7 @@ main:
     neg dword [ball_speed_x]
     
 .check_paddle2:
-    ; Pravé pádlo
+    ; Right paddle
     mov eax, [ball_x]
     add eax, [ball_size]
     cmp eax, [player2_x]
@@ -164,33 +161,47 @@ main:
     
     neg dword [ball_speed_x]
     
-    ; --- Kontrola gólů ---
+    ; --- Goal detection ---
 .check_goals:
-    ; Levý okraj
+    ; Left goal
     mov eax, [ball_x]
     cmp eax, 0
     jg .check_right_goal
     inc dword [player2_score]
+    
+    ; Update score text
+    mov rdi, player2_text
+    mov rsi, score_format
+    mov edx, [player2_score]
+    call sprintf
+    
     call reset_ball
     
 .check_right_goal:
-    ; Pravý okraj
+    ; Right goal
     mov eax, [ball_x]
     mov ebx, [screen_width]
     cmp eax, ebx
     jl .draw
     inc dword [player1_score]
+    
+    ; Update score text
+    mov rdi, player1_text
+    mov rsi, score_format
+    mov edx, [player1_score]
+    call sprintf
+    
     call reset_ball
     
-    ; --- Vykreslení ---
+    ; --- Drawing ---
 .draw:
     call BeginDrawing
     
-    ; Černé pozadí
+    ; Black background
     mov edi, BLACK
     call ClearBackground
     
-    ; Levé pádlo
+    ; Left paddle
     mov edi, [player1_x]
     mov esi, [player1_y]
     mov edx, [paddle_width]
@@ -198,7 +209,7 @@ main:
     mov r8d, WHITE
     call DrawRectangle
     
-    ; Pravé pádlo
+    ; Right paddle
     mov edi, [player2_x]
     mov esi, [player2_y]
     mov edx, [paddle_width]
@@ -206,7 +217,7 @@ main:
     mov r8d, WHITE
     call DrawRectangle
     
-    ; Míček
+    ; Ball
     mov edi, [ball_x]
     mov esi, [ball_y]
     mov edx, [ball_size]
@@ -214,33 +225,27 @@ main:
     mov r8d, WHITE
     call DrawRectangle
     
-    ; Skóre hráče 1
-    mov rdi, score_text
-    mov rsi, score_format
-    mov edx, [player1_score]
-    call sprintf
-    
-    mov rdi, score_text
-    mov esi, 10
-    mov edx, 200
-    mov ecx, 30
+    ; Player 1 score (left side)
+    mov rdi, player1_text
+    mov esi, [screen_width]
+    shr esi, 2      ; screen_width / 4
+    mov edx, 30     ; Y position
+    mov ecx, 40     ; Font size
     mov r8d, WHITE
     call DrawText
     
-    ; Skóre hráče 2
-    mov rdi, score_text
-    mov rsi, score_format
-    mov edx, [player2_score]
-    call sprintf
-    
-    mov rdi, score_text
-    mov esi, 10
-    mov edx, 600
-    mov ecx, 30
+    ; Player 2 score (right side)
+    mov rdi, player2_text
+    mov esi, [screen_width]
+    shr esi, 1      ; screen_width / 2
+    add esi, [screen_width]
+    shr esi, 1      ; screen_width * 3/4
+    mov edx, 30     ; Y position
+    mov ecx, 40     ; Font size
     mov r8d, WHITE
     call DrawText
     
-    ; FPS
+    ; FPS counter
     mov edi, 10
     mov esi, 10
     call DrawFPS
@@ -252,14 +257,14 @@ main:
 .close_window:
     call CloseWindow
     
-    ; Ukončení programu
+    ; Exit program
     mov rsp, rbp
     pop rbp
     xor eax, eax
     ret
     
 reset_ball:
-    ; Reset míčku do středu
+    ; Reset ball to center
     mov eax, [screen_width]
     shr eax, 1
     mov [ball_x], eax
@@ -268,7 +273,7 @@ reset_ball:
     shr eax, 1
     mov [ball_y], eax
     
-    ; Náhodný směr (pro zjednodušení jen obrácení směru)
+    ; Reverse direction
     neg dword [ball_speed_x]
     mov dword [ball_speed_y], 4
     ret
